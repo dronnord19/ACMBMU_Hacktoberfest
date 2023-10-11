f9c33cf7f65c770422922e146fb99d04ec20a82b
dronnord19 <147618839+dronnord19@users.noreply.github.com>
2023-10-11 18:22:40 +0530

    Create dron_230612_2.cpp

---
+++ dron_230612_2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j;
+    for(i=0;i<=5;i++)
+    {
+        for(j=0;j<=9;j++)
+        {
+            if(j<=4+i && j>=6-i)
+            {
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+           
+        }
+        cout<<endl;
+    }  
+}
